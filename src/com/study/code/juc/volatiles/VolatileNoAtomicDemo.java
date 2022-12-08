a647e798140c4c4e53012d4c570c1691700a4dde
Jie <jieliang@flamingo.shop>
2022-12-08 11:40:29 +0800

    1

--- src/com/study/code/juc/volatiles/VolatileNoAtomicDemo.java
+++ src/com/study/code/juc/volatiles/VolatileNoAtomicDemo.java
@@ -12,7 +12,7 @@ class MyNumber {
     // int number ;
     volatile int number;
 
-    // public synchronized void addPlusPlus() {
+    // public synch ronized void addPlusPlus() {
     public void addPlusPlus() {
         number++;
     }
